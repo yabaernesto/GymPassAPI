ec06ab8c3ac474989824963d98d015fc586a8101
Yaba Ernesto <yabaernesto@gmail.com>
2025-02-10 23:26:25 +0100

    feat: Repository Pattern  de register adicionado

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,5 +1,6 @@
 import { hash } from 'bcryptjs'
 import { prisma } from '@/lib/prisma'
+import { PrismaUsersRepository } from '@/repositories/prisma-users-repository'
 
 interface RegisterUseCaseRequest {
   name: string
@@ -24,11 +25,11 @@ export async function registerUseCase({
     throw new Error('e-mail already exists.')
   }
 
-  await prisma.user.create({
-    data: {
-      name,
-      email,
-      password_hash,
-    },
+  const prismaUsersRepository = new PrismaUsersRepository()
+
+  await prismaUsersRepository.create({
+    name,
+    email,
+    password_hash,
   })
 }
