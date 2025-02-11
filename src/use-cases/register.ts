5f276f9b58216d4cfe4dd1a8d544b5f5207283de
Yaba Ernesto <yabaernesto@gmail.com>
2025-02-11 02:22:21 +0100

    feat: Tratamento de erros adicionado

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,5 +1,6 @@
 import { hash } from 'bcryptjs'
 import { UsersRepository } from '@/repositories/users-repository'
+import { UserAlreadyExistsError } from './errors/user-already-exists-error'
 
 interface RegisterUseCaseRequest {
   name: string
@@ -16,7 +17,7 @@ export class RegisterUseCase {
     const userWhiteSameEmail = await this.usersRepository.findByEmail(email)
 
     if (userWhiteSameEmail) {
-      throw new Error('e-mail already exists.')
+      throw new UserAlreadyExistsError()
     }
 
     await this.usersRepository.create({
