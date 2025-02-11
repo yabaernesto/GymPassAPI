eb2accce31abd7db6310cdef917ee29d492327c7
Yaba Ernesto <yabaernesto@gmail.com>
2025-02-11 02:41:59 +0100

    feat: Tratamento de erros Globais

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,22 @@
 import fastify from 'fastify'
+import { ZodError } from 'zod'
 import { appRoutes } from './http/routes'
+import { env } from './env'
 
 export const app = fastify()
 
 app.register(appRoutes)
+
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    return reply
+      .status(400)
+      .send({ message: 'Validation error.', issues: error.format() })
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.log(error)
+  }
+
+  return reply.status(500).send({ message: 'Internal server error.' })
+})
