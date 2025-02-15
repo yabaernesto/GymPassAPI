c56b63598cad511bb85330b26bd98c1a8f61b911
Yaba Ernesto <yabaernesto@gmail.com>
2025-02-15 04:41:48 +0100

    test: Caso de Uso de pesquisa de Perfil

---
+++ src/use-cases/get-user-profile.spec.ts
@@ -0,0 +1,37 @@
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
+import { describe, it, beforeEach, expect } from 'vitest'
+import { GetUserPerfilUseCase } from './get-user-perfil'
+import { hash } from 'bcryptjs'
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
+
+let usersRepository: InMemoryUsersRepository
+let sut: GetUserPerfilUseCase
+
+describe('Get Use Profile Use Case', () => {
+  beforeEach(() => {
+    usersRepository = new InMemoryUsersRepository()
+    sut = new GetUserPerfilUseCase(usersRepository)
+  })
+
+  it('shoud be able to get profile', async () => {
+    const createdUser = await usersRepository.create({
+      name: 'Yaba Ernesto',
+      email: 'yaba@gmail.com',
+      password_hash: await hash('123456', 6),
+    })
+
+    const { user } = await sut.execute({
+      userId: createdUser.id,
+    })
+
+    expect(user.name).toEqual('Yaba Ernesto')
+  })
+
+  it('shoud not be able to get user profile with wrong id', async () => {
+    expect(() =>
+      sut.execute({
+        userId: 'non-existing-id',
+      }),
+    ).rejects.toBeInstanceOf(ResourceNotFoundError)
+  })
+})
