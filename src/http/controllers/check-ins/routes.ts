a1a096f7c4fc9c2e2e1f8cfbccbfd834df08af8a
Yaba Ernesto <yabaernesto@gmail.com>
2025-03-04 10:50:55 +0100

    refactor: nome da rota refatorada

--- src/http/controllers/check-ins/routes.ts
+++ src/http/controllers/check-ins/routes.ts
@@ -6,7 +6,7 @@ import { validate } from './validate'
 import { history } from './history'
 import { metrics } from './metrics'
 
-export async function gymsRoutes(app: FastifyInstance) {
+export async function checkInRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJWT)
 
   app.get('/check-ins/history', history)
