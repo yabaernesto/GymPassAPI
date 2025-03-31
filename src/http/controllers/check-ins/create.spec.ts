415639cdea863623c7c7b8b9cfd8b53481829831
Yaba Ernesto <yabaernesto@gmail.com>
2025-03-31 23:47:08 +0100

    test: testes e2e dos controllers de check-ins

---
+++ src/http/controllers/check-ins/create.spec.ts
@@ -0,0 +1,41 @@
+import request from 'supertest'
+import { app } from '@/app'
+import { prisma } from '@/lib/prisma'
+
+import { describe, it, expect, beforeAll, afterAll } from 'vitest'
+import { createAndAuthenticateUser } from '@/utils/test/create-and-authenticate-user'
+
+describe('Create Check-in (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to create a check-in', async () => {
+    const { token } = await createAndAuthenticateUser(app)
+
+    const gym = await prisma.gym.create({
+      data: {
+        title: 'JavaScript Gym',
+        latitude: -27.2092052,
+        longitude: -49.6401091,
+      },
+    })
+
+    const response = await request(app.server)
+      .post(`/gyms/${gym.id}/check-ins`)
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'JavaScript Gym',
+        description: 'Some description',
+        phone: '119999999999',
+        latitude: -27.2092052,
+        longitude: -49.6401091,
+      })
+
+    expect(response.statusCode).toEqual(201)
+  })
+})
